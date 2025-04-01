a89611cc36248d6c5a592832fc679dc44460b959
isuretpolos <isuret.polos@gmail.com>
2025-04-01 21:12:17 +0200

    Generate Hotbits and deliver over REST using all available CPUs and also deliver static files as an http server

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 ![alt](go/images/aetheronego.jpg)
 Open Source Radionics written in Golang and three.js
+
+# Download
+I will release compiled versions for different operating systems [here](https://github.com/isuretpolos/AetherOneGo/releases/latest).
+
+# Build
+If you want to build your own version, you need to have the following installed:
+- Go, see [here](https://golang.org/doc/install)
+
+And then run the following commands:
+```bash
+go get -u github.com/isuretpolos/AetherOneGo
+cd $GOPATH/src/github.com/isuretpolos/AetherOneGo
+go build
+```
+
--- go/main.go
+++ go/main.go
@@ -2,154 +2,121 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
-	"io"
+	"log"
+	"math/rand"
 	"net/http"
-	"os"
-	"os/exec"
+	"path/filepath"
 	"runtime"
-	"strings"
+	"strconv"
 	"time"
 )
 
-const (
-	currentVersion = "v0.0.0"
-	owner          = "isuretpolos"
-	repo           = "AetherOneGo"
-)
-
-type Asset struct {
-	Name string `json:"name"`
-	URL  string `json:"browser_download_url"`
+type Result struct {
+	Numbers []uint32 `json:"numbers"`
 }
 
-type Release struct {
-	TagName string  `json:"tag_name"`
-	Assets  []Asset `json:"assets"`
+// measureRandomBitInteger generates a 32-bit integer based on loop timing differences
+func measureRandomBitInteger(iterations int) uint32 {
+	bits := make([]byte, 0, 32)
+
+	for len(bits) < 32 {
+		start1 := time.Now()
+		for i := 0; i < iterations; i++ {
+			_ = rand.Intn(10+1) * rand.Intn(10+1)
+		}
+		end1 := time.Now()
+
+		start2 := time.Now()
+		for i := 0; i < iterations; i++ {
+			_ = rand.Intn(10+1) * rand.Intn(10+1)
+		}
+		end2 := time.Now()
+
+		if end1.Sub(start1) < end2.Sub(start2) {
+			bits = append(bits, '1')
+		} else {
+			bits = append(bits, '0')
+		}
+
+		time.Sleep(time.Millisecond) // slight pause between iterations
+	}
+
+	// Convert bit string to integer
+	bitString := string(bits)
+	value, _ := strconv.ParseUint(bitString, 2, 32)
+	return uint32(value)
+}
+
+func generateHandler(w http.ResponseWriter, r *http.Request) {
+	countStr := r.URL.Query().Get("count")
+	loopsStr := r.URL.Query().Get("loops")
+
+	count := 1
+	loops := 50
+
+	if countStr != "" {
+		if parsed, err := strconv.Atoi(countStr); err == nil && parsed > 0 {
+			count = parsed
+		}
+	}
+	if loopsStr != "" {
+		if parsed, err := strconv.Atoi(loopsStr); err == nil && parsed > 0 {
+			loops = parsed
+		}
+	}
+
+	type jobResult struct {
+		index int
+		value uint32
+	}
+
+	results := make([]uint32, count)
+	resultChan := make(chan jobResult)
+
+	// Launch goroutines
+	for i := 0; i < count; i++ {
+		go func(idx int) {
+			value := measureRandomBitInteger(loops)
+			resultChan <- jobResult{index: idx, value: value}
+		}(i)
+	}
+
+	// Collect results
+	for i := 0; i < count; i++ {
+		res := <-resultChan
+		results[res.index] = res.value
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	prettyJSON, err := json.MarshalIndent(Result{Numbers: results}, "", "  ")
+	if err != nil {
+		http.Error(w, "Failed to encode JSON", http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(prettyJSON)
 }
 
 func main() {
-	release, err := fetchLatestRelease(owner, repo)
-	if err != nil {
-		fmt.Println("Fehler beim Abrufen der Release:", err)
-		return
-	}
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	port := flag.Int("port", 8080, "Port to serve on")
+	staticDir := flag.String("static", "./static", "Directory to serve static files from")
+	flag.Parse()
 
-	if release.TagName != currentVersion {
-		fmt.Printf("Neue Version gefunden: %s\n", release.TagName)
-		asset := findAssetForOS(release)
-		if asset == nil {
-			fmt.Println("Kein passendes Asset für dieses OS gefunden.")
-			return
-		}
+	rand.Seed(time.Now().UnixNano())
 
-		tempFile := "app_update_tmp"
-		if runtime.GOOS == "windows" {
-			tempFile += ".exe"
-		}
+	// Serve static files
+	fs := http.FileServer(http.Dir(filepath.Clean(*staticDir)))
+	http.Handle("/", fs)
 
-		fmt.Println("Lade Update herunter:", asset.Name)
-		err = downloadFile(asset.URL, tempFile)
-		if err != nil {
-			fmt.Println("Fehler beim Download:", err)
-			return
-		}
+	// REST API
+	http.HandleFunc("/generate", generateHandler)
 
-		err = os.Chmod(tempFile, 0755)
-		if err != nil {
-			fmt.Println("Fehler beim Setzen der Berechtigungen:", err)
-			return
-		}
-
-		startUpdater(tempFile)
-		os.Exit(0)
-	} else {
-		fmt.Println("Aktuelle Version ist auf dem neuesten Stand.")
-	}
-}
-
-func fetchLatestRelease(owner, repo string) (*Release, error) {
-
-	url := fmt.Sprintf("https://api.github.com/repos/%s/%s/releases/latest", owner, repo)
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	var rel Release
-	if err := json.NewDecoder(resp.Body).Decode(&rel); err != nil {
-		return nil, err
-	}
-	return &rel, nil
-}
-
-func findAssetForOS(rel *Release) *Asset {
-	osStr := runtime.GOOS
-	archStr := runtime.GOARCH
-
-	for _, asset := range rel.Assets {
-		if strings.Contains(asset.Name, osStr) && strings.Contains(asset.Name, archStr) {
-			return &asset
-		}
-	}
-	return nil
-}
-
-func downloadFile(url, path string) error {
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	file, err := os.Create(path)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	_, err = io.Copy(file, resp.Body)
-	return err
-}
-
-func startUpdater(tempFile string) {
-	var cmd *exec.Cmd
-	executable, _ := os.Executable()
-
-	if runtime.GOOS == "windows" {
-		cmd = exec.Command(tempFile, "update", executable)
-	} else {
-		cmd = exec.Command("./"+tempFile, "update", executable)
-	}
-
-	cmd.Start()
-}
-
-// Update-Prozess abhandeln
-func init() {
-	if len(os.Args) == 3 && os.Args[1] == "update" {
-		originalFile := os.Args[2]
-		updateProcess(originalFile)
-	}
-}
-
-func updateProcess(original string) {
-	time.Sleep(2 * time.Second)
-
-	err := os.Remove(original)
-	if err != nil {
-		fmt.Println("Fehler beim Löschen der alten Version:", err)
-		return
-	}
-
-	err = os.Rename(os.Args[0], original)
-	if err != nil {
-		fmt.Println("Fehler beim Umbenennen der neuen Version:", err)
-		return
-	}
-
-	cmd := exec.Command(original)
-	cmd.Start()
-	os.Exit(0)
+	addr := fmt.Sprintf(":%d", *port)
+	log.Printf("Serving on http://localhost%s (static folder: %s)", addr, *staticDir)
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
